9cd9f91576f7ab640431e166efcac19bf4f6f547
Untitled <john.d.reddock@gmail.com>
2018-03-08 13:58:01 -0500

    implement updating of product

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -17,4 +17,9 @@ export class ProductService {
   get(productId){
     return this.db.object('/products/' + productId)
   }
+
+  update(productId, product){
+    return this.db.object('/products/' + productId).update(product);
+  }
+  
 }
