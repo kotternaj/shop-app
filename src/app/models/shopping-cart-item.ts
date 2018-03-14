d5e793b3da1fddb2ba83a2bf6b0385fadddbae01
Untitled <john.d.reddock@gmail.com>
2018-03-14 15:21:34 -0400

    displaying total price

--- src/app/models/shopping-cart-item.ts
+++ src/app/models/shopping-cart-item.ts
@@ -1,8 +1,23 @@
-import { Product } from "./product";
+import { Product } from './product';
 
-export class ShoppingCartItem{
-  product: Product;
-  quantity: number;
-  
-  get totalPrice(){ return this.product.price * this.quantity; }
+export class ShoppingCartItem {
+  $key: string;
+  title: string;
+  imageUrl: string;
+  price: number; 
+  quantity: number; 
+
+  constructor(init?: Partial<ShoppingCartItem>) {
+    Object.assign(this, init);
+  }
+
+  get totalPrice() { return this.price * this.quantity; }
 }
+
+// import { Product } from "./product";
+
+// export class ShoppingCartItem {
+//   constructor(public product: Product, public quantity: number) {}
+
+//   get totalPrice(){ return this.product.price * this.quantity; }
+// }
--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,17 +1,55 @@
-import { ShoppingCartItem } from "./shopping-cart-item";
+import { Product } from './product';
+import { ShoppingCartItem } from './shopping-cart-item';
 
-export class ShoppingCart {
-  items: ShoppingCartItem[] =[];
+export class ShoppingCart { 
+  items: ShoppingCartItem[] = [];
 
-  constructor(public itemsMap: {[productId: string]: ShoppingCartItem}) {
-      for (let productId in itemsMap)
-          this.items.push(itemsMap[productId]);
-      }  
-
-    get totalItemsCount(){
-        let count = 0;  
-        for (let productId in this.items)
-        count += this.items[productId].quantity;    
-        return count;
+  constructor(private itemsMap: { [productId: string]: ShoppingCartItem }) {
+    this.itemsMap = itemsMap || {};
+    
+    for (let productId in itemsMap) {
+      let item = itemsMap[productId];
+      this.items.push(new ShoppingCartItem({ ...item, $key: productId }));
     }
+  }
+
+  getQuantity(product: Product) {
+    let item = this.itemsMap[product.$key];
+    return item ? item.quantity : 0;
+  }
+  
+  get totalPrice() {
+    let sum = 0;
+    for (let productId in this.items) 
+      sum += this.items[productId].totalPrice;
+    return sum;
+  }
+  
+  get totalItemsCount() {
+    let count = 0;
+    for (let productId in this.itemsMap) 
+      count += this.itemsMap[productId].quantity;
+    return count;
+  }
 }
+
+// import { ShoppingCartItem } from "./shopping-cart-item";
+
+// export class ShoppingCart {
+//   items: ShoppingCartItem[] =[];
+
+//   constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+//       for (let productId in itemsMap) {
+//         let item = itemsMap[productId]; 
+//         this.items.push(itemsMap[productId]);
+//         this.items.push(new ShoppingCartItem(item.product, item.quantity));
+//       }          
+//     }  
+
+//     get totalItemsCount(){
+//         let count = 0;  
+//         for (let productId in this.items)
+//         count += this.items[productId].quantity;    
+//         return count;
+//     }
+// }
